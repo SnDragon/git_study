e9c4cb6f6b7159a5045bfeeff1bd9e1124820e46
SnDragon <1803240383@qq.com>
2019-03-03 09:50:07 +0800

    hello

---
+++ hello.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: longerwu
+ * Date: 2019/2/17
+ * Time: 9:56
+ */
+
+class A{
+    public function helo()
+    {
+        
+    }
+
+    public function hello($i)
+    {
+
+    }
+}
+
+
+function test($lastName){
+    test2();
+}
+
+function test2(){
+    echo '111';
+}
+
+test();
